2a33c4f6f58f50bcba762982850f0f40e3044a2c
Vitaliy Yurakh <109657047+VitaliyYurakh@users.noreply.github.com>
2023-06-04 16:49:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,580 @@
+const  allClub = {
+    english: [
+        {
+            id: 1,
+            name: 'Liverpool',
+            src: 'icons/Liverpool.png',
+            label: 'Ліверпуль'
+        },
+        {
+            id: 2,
+            name: 'Tottenham',
+            src: 'icons/Tottenham.png',
+            label: 'Тотенхем'
+        },
+        {
+            id: 3,
+            name: 'Manchester City',
+            src: 'icons/Manchester City.png',
+            label: 'Манчестер Сіті'
+        },
+        {
+            id: 4,
+            name: 'Manchester_United',
+            src: 'icons/Manchester_United.png',
+            label: 'Манчестер Юнайтед'
+        },
+        {
+            id: 5,
+            name: 'Arsenal',
+            src: 'icons/Arsenal.png',
+            label: 'Арсенал'
+        },
+        {
+            id: 6,
+            name: 'West_Ham',
+            src: 'icons/West_Ham.png',
+            label: 'Вест Хем'
+        },
+        {
+            id: 7,
+            name: 'Chelsea',
+            src: 'icons/Chelsea.png',
+            label: 'Челси'
+        },
+        {
+            id: 8,
+            name: 'Everton',
+            src: 'icons/Everton.png',
+            label: 'Евертон'
+        },
+        {
+            id: 9,
+            name: 'Wolves',
+            src: 'icons/Wolver.png',
+            label: 'Wolves'
+        },
+        {
+            id: 10,
+            name: 'Leiceste',
+            src: 'icons/Leiceste.png',
+            label: 'Лестер'
+        },
+        {
+            id: 11,
+            name: 'Crystal_Palace',
+            src: 'icons/Crystal_Palace.png',
+            label: 'Crystal Palace'
+        },
+    ],
+    italy: [
+        {
+            id: 12,
+            name: 'Juventus',
+            src: 'icons/Juventus.png',
+            label: 'Ювентус'
+        },
+        {
+            id: 13,
+            name: 'Internazionale_Milano',
+            src: 'icons/Internazionale_Milano.png',
+            label: 'Інтер'
+        },
+        {
+            id: 14,
+            name: 'Neapel',
+            src: 'icons/Neapel.png',
+            label: 'Наполі'
+        },
+        {
+            id: 15,
+            name: 'Lazio',
+            src: 'icons/Lazio.png',
+            label: 'Лаціо'
+        },
+        {
+            id: 16,
+            name: 'Milan',
+            src: 'icons/Milan.png',
+            label: 'Мілан'
+        },
+        {
+            id: 17,
+            name: 'Roma',
+            src: 'icons/Roma.png',
+            label: 'Рома'
+        },
+        {
+            id: 18,
+            name: 'Torino',
+            src: 'icons/Torino.png',
+            label: 'Торіно'
+        },
+    ],
+    france: [
+        {
+            id: 32,
+            name: 'Paris_Saint-Germain',
+            src: 'icons/Paris_Saint-Germain.png',
+            label: 'ПСЖ'
+        },
+        {
+            id: 33,
+            name: 'Lyonnais',
+            src: 'icons/Lyonnais.png',
+            label: 'Ліон'
+        },
+        {
+            id: 34,
+            name: 'Marseille',
+            src: 'icons/Marseille.png',
+            label: 'Марсель'
+        },
+        {
+            id: 35,
+            name: 'Monaco',
+            src: 'icons/Monaco.png',
+            label: 'Монако'
+        },
+    ],
+    spain: [
+        {
+            id: 23,
+            name: 'Real_Madrid',
+            src: 'icons/Real_Madrid.png',
+            label: 'Реал Мадрид'
+        },
+        {
+            id: 24,
+            name: 'Barcelona',
+            src: 'icons/Barcelona.png',
+            label: 'Барселона'
+        },
+        {
+            id: 25,
+            name: 'Atletico_Madrid',
+            src: 'icons/Atletico_Madrid.png',
+            label: 'Атлетико Мадрид'
+        },
+        {
+            id: 26,
+            name: 'Valencia',
+            src: 'icons/Valencia.png',
+            label: 'Валенсія'
+        },
+        {
+            id: 27,
+            name: 'Sevilla',
+            src: 'icons/Sevilla.webp',
+            label: 'Севілія'
+        },
+        {
+            id: 28,
+            name: 'Athletic_Bilbao',
+            src: 'icons/Athletic_Bilbao.png',
+            label: 'Атлетико Більбао'
+        },
+        {
+            id: 29,
+            name: 'Celta',
+            src: 'icons/Celta.png',
+            label: 'Celta'
+        },
+        {
+            id: 30,
+            name: 'Betis',
+            src: 'icons/Betis.png',
+            label: 'Реал Бетіс'
+        },
+        {
+            id: 31,
+            name: 'Real_Sociedad',
+            src: 'icons/Real_Sociedad.png',
+            label: 'Реал Сосьєдад'
+        },
+    ],
+    germany: [
+        {
+            id: 32,
+            name: 'Bayern_Munchen',
+            src: 'icons/Bayern_Munchen.webp',
+            label: 'Баварія'
+        },
+        {
+            id: 33,
+            name: 'Borussia_Dortmund',
+            src: 'icons/Borussia_Dortmund.png',
+            label: 'Борусія Д.'
+        },
+        {
+            id: 34,
+            name: 'RB_Leipzig',
+            src: 'icons/RB_Leipzig.png',
+            label: 'Red Bull'
+        },
+        {
+            id: 35,
+            name: 'Schalke',
+            src: 'icons/Schalke.png',
+            label: 'Шальке'
+        },
+        {
+            id: 36,
+            name: 'Bayer',
+            src: 'icons/Bayer.png',
+            label: 'Байерн'
+        },
+        {
+            id: 37,
+            name: 'Borussia_Moenchengladbach',
+            src: 'icons/Borussia_Moenchengladbach.png',
+            label: 'Борусія М.'
+        },
+        {
+            id: 38,
+            name: 'Werder',
+            src: 'icons/Werder.png',
+            label: 'Вердер'
+        },
+    ],
+    portugal: [
+        {
+            id: 39,
+            name: 'Porto',
+            src: 'icons/Porto.png',
+            label: 'Порту'
+        },
+        {
+            id: 40,
+            name: 'Benfica',
+            src: 'icons/Benfica.png',
+            label: 'Бенфіка'
+        },
+        {
+            id: 41,
+            name: 'Sporting',
+            src: 'icons/Sporting.png',
+            label: 'Спортінг'
+        },
+        {
+            id: 42,
+            name: 'Braga',
+            src: 'icons/Braga.png',
+            label: 'Брага'
+        },
+    ],
+    other: [
+        {
+            id: 43,
+            name: 'Ajax',
+            src: './icons/Ajax.png',
+            label: 'Аякс'
+        },
+        {
+            id: 44,
+            name: 'Shakhtar',
+            src: './icons/Shakhtar.png',
+            label: 'Шахтар'
+        },
+        {
+            id: 45,
+            name: 'Dynamo_Kyiv',
+            src: './icons/Dynamo_Kyiv.png',
+            label: 'Динамо'
+        },
+        {
+            id: 46,
+            name: 'Besiktas',
+            src: './icons/Besiktas.png',
+            label: 'Бешікташ'
+        },
+        {
+            id: 47,
+            name: 'Fenerbahce',
+            src: './icons/Fenerbahce.png',
+            label: 'Фенехбахче'
+        },
+        {
+            id: 48,
+            name: 'Galatasaray',
+            src: './icons/Galatasaray.png',
+            label: 'Галатасарай'
+        },
+    ]
+}
+
+
+const topField = document.querySelector('.top')
+const playerOne = document.querySelector('#player_one')
+const playerTwo = document.querySelector('#player_two')
+const allBtnReturn = document.querySelectorAll('.return')
+const allBtnLose= document.querySelectorAll('.lose')
+
+
+topField.addEventListener('click', handleClickOnTop)
+allBtnReturn.forEach(btn => btn.addEventListener('click', clearFieldPlayer))
+allBtnLose.forEach(btn => btn.addEventListener('click', removeClub))
+
+function handleClickOnTop({ target }) {
+    if (target.classList.contains('item')) {
+
+        const nameClub = target.dataset.club
+        const labelClub = target.textContent.trim()
+        const srcClub = target.children[0].src
+        const playerClub = {
+            name: nameClub,
+            label: labelClub,
+            src: srcClub
+        }
+        const playerClubSelect = createClub(playerClub)
+
+        if (!playerOne.children.length) {
+            playerOne.insertAdjacentHTML('beforeend', playerClubSelect)
+            postPlayerOnDatabase('playerOne', playerClub)
+        } else if (!playerTwo.children.length) {
+            playerTwo.insertAdjacentHTML('beforeend', playerClubSelect)
+            postPlayerOnDatabase('playerTwo', playerClub)
+        }
+
+    }
+}
+
+function clearFieldPlayer(event) {
+
+    const { target } = event
+
+    if ( target.dataset.id === 'one' ) {
+        playerOne.innerHTML = ''
+        removePlayerInDatabase('playerOne')
+    }
+
+    if ( target.dataset.id === 'two' ) {
+        playerTwo.innerHTML = ''
+        removePlayerInDatabase('playerTwo')
+    }
+} 
+
+function removeClub(event) {
+    const { target } = event
+
+    const isSure = confirm('Видалити команду')
+    
+    if (isSure) {
+        if ( target.dataset.id === 'one' && playerOne.innerText !== '') {
+            const club = playerOne.children[0]
+            removePlayerInDatabase('playerOne')
+            removeClubFromList(club)
+        }
+    
+        if ( target.dataset.id === 'two' && playerTwo.innerText !== '') {
+            const club = playerTwo.children[0]
+            removePlayerInDatabase('playerTwo')
+            removeClubFromList(club)
+        }
+        
+        clearFieldPlayer(event)
+    }
+}
+
+
+function initPlayer(one, two) {
+    const isValidOne = Object.keys(one).length > 0
+    const isValidTwo = Object.keys(two).length > 0
+
+    if (isValidOne) { 
+        const player = createClub(one)
+        playerOne.insertAdjacentHTML('beforeend', player)
+    }
+
+    if (isValidTwo) {
+        const player = createClub(two)
+        playerTwo.insertAdjacentHTML('beforeend', player)
+    }
+}
+
+function init(allFootbalClub) {
+    const countries = Object.keys(allFootbalClub)
+
+    countries.forEach(country => {
+        const countrySelectContainer = document.querySelector(`[data-country="${country}"]`)
+        countrySelectContainer.innerHTML=""
+        renderClubs(countrySelectContainer, country, allFootbalClub)
+    })
+}
+
+function renderClubs(container, country, allFootbalClub) {
+    let clubsFragment = ''
+
+    allFootbalClub[country].forEach(club => {
+        const item = createClub(club)
+        clubsFragment +=item
+    })
+
+    container.insertAdjacentHTML('beforeend', clubsFragment)
+
+}
+
+function createClub({ name, src, label }) {
+    return `
+        <div class="item" data-club="${name}">
+            <img src="${src}" alt="">${label}
+        </div>
+    `
+}
+
+getClubs()
+getPlayerOnDatabase()
+
+// Work with JSON-Server
+
+const btnReset = document.querySelector('.reset')
+
+btnReset.addEventListener('click', postClubs)
+
+async function postClubs() {
+    try {
+        const data = JSON.stringify(allClub)
+        const res = await fetch('http://localhost:3000/allFootbalClub', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        })
+        if (!res.ok) {
+            throw new Error()
+        }
+        
+        getClubs()
+
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+async function getClubs() {
+    try {
+        const res = await fetch('http://localhost:3000/allFootbalClub', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+
+        const allFootbalClub = await res.json()
+
+        if (!res.ok) {
+            throw new Error()
+        }
+
+        init(allFootbalClub)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function postPlayerOnDatabase(player, playerClub) {
+    try {
+        const data = JSON.stringify(playerClub)
+        const res = await fetch(`http://localhost:3000/${player}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        })
+
+        if (!res.ok) {
+            throw new Error('test')
+        }
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+async function getPlayerOnDatabase() {
+    try {
+        const resPlayerOne = await fetch('http://localhost:3000/playerOne', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+
+        const resPlayerTwo = await fetch('http://localhost:3000/playerTwo', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+
+        const playerOne = await resPlayerOne.json()
+        const playerTwo = await resPlayerTwo.json()
+
+        if (!resPlayerOne.ok || !resPlayerTwo.ok) {
+            throw new Error()
+        }
+
+        initPlayer(playerOne, playerTwo)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function removePlayerInDatabase(player) {
+    try {
+        const res = await fetch(`http://localhost:3000/${player}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({})
+        })
+
+        if (!res.ok) {
+            throw new Error()
+        }
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+async function removeClubFromList(club) {
+    try {
+        const response = await fetch('http://localhost:3000/allFootbalClub', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+
+        const allFootbalClub = await response.json()
+
+        const clubName = club.dataset.club
+        const clubDelete = document.querySelector(`[data-club='${clubName}']`)
+        const country = clubDelete.parentElement.dataset.country
+
+        allFootbalClub[country].forEach((club, idx) => {
+            if (club.name === clubName) {
+                allFootbalClub[country].splice(idx, 1)
+            }
+        })
+
+        clubDelete.remove()
+
+        const res = await fetch('http://localhost:3000/allFootbalClub', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(allFootbalClub)
+        })
+
+        if (!res.ok) {
+            throw new Error()
+        }
+
+    } catch (err) {
+        alert(err.message);
+    }
+}
